152363e1fb611b8d66101ade41b3f4f080125c39
Xalalau <xalalau@outlook.com>
2025-07-19 18:07:49 -0300

    Adicionar monster_scientist_falsificado

--- dlls/scientist.cpp
+++ dlls/scientist.cpp
@@ -1453,6 +1453,8 @@ bool CSittingScientist::FIdleSpeak()
 
 // ############ hu3lifezado ############ //
 // Novos cientistas
+
+// Cientista gigante
 class CScientistGiant : public CScientist
 {
 public:
@@ -1524,6 +1526,91 @@ void CScientistGiant::Spawn(void)
 void CScientistGiant::Precache(void)
 {
 	PRECACHE_MODEL("models/scientist_gigante.mdl");
+	PRECACHE_SOUND("scientist/sci_pain1.wav");
+	PRECACHE_SOUND("scientist/sci_pain2.wav");
+	PRECACHE_SOUND("scientist/sci_pain3.wav");
+	PRECACHE_SOUND("scientist/sci_pain4.wav");
+	PRECACHE_SOUND("scientist/sci_pain5.wav");
+
+	// every new scientist must call this, otherwise
+	// when a level is loaded, nobody will talk (time is reset to 0)
+	TalkInit();
+
+	CTalkMonster::Precache();
+}
+
+// Cientista falsificado
+class CScientistParaguai : public CScientist
+{
+public:
+	bool Save(CSave& save) override;
+	bool Restore(CRestore& restore) override;
+	static TYPEDESCRIPTION m_SaveData[];
+
+	void Spawn(void);
+	void Precache(void);
+
+private:
+	float m_painTime;
+	float m_healTime;
+	float m_fearTime;
+};
+
+TYPEDESCRIPTION CScientistParaguai::m_SaveData[] =
+{
+	DEFINE_FIELD(CScientistParaguai, m_painTime, FIELD_TIME),
+	DEFINE_FIELD(CScientistParaguai, m_healTime, FIELD_TIME),
+	DEFINE_FIELD(CScientistParaguai, m_fearTime, FIELD_TIME),
+};
+
+IMPLEMENT_SAVERESTORE(CScientistParaguai, CScientist);
+
+LINK_ENTITY_TO_CLASS(monster_scientist_falsificado, CScientistParaguai);
+
+//=========================================================
+// Spawn
+//=========================================================
+void CScientistParaguai::Spawn(void)
+{
+	Precache();
+
+	SET_MODEL(ENT(pev), "models/scientist_falsificado.mdl");
+	UTIL_SetSize(pev, VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX);
+
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_RED;
+	pev->health = gSkillData.scientistHealth;
+	pev->view_ofs = Vector(0, 0, 50);// position of the eyes relative to monster's origin.
+	m_flFieldOfView = VIEW_FIELD_WIDE; // NOTE: we need a wide field of view so scientists will notice player and say hello
+	m_MonsterState = MONSTERSTATE_NONE;
+
+	//	m_flDistTooFar		= 256.0;
+
+	m_afCapability = bits_CAP_HEAR | bits_CAP_TURN_HEAD | bits_CAP_OPEN_DOORS | bits_CAP_AUTO_DOORS | bits_CAP_USE;
+
+	// White hands
+	pev->skin = 0;
+
+	if (pev->body == -1)
+	{// -1 chooses a random head
+		pev->body = RANDOM_LONG(0, NUM_SCIENTIST_HEADS - 1);// pick a head, any head
+	}
+
+	// Luther is black, make his hands black
+	if (pev->body == HEAD_LUTHER)
+		pev->skin = 1;
+
+	MonsterInit();
+	SetUse(&CScientist::FollowerUse);
+}
+
+//=========================================================
+// Precache - precaches all resources this monster needs
+//=========================================================
+void CScientistParaguai::Precache(void)
+{
+	PRECACHE_MODEL("models/scientist_falsificado.mdl");
 	PRECACHE_SOUND("scientist/sci_pain1.wav");
 	PRECACHE_SOUND("scientist/sci_pain2.wav");
 	PRECACHE_SOUND("scientist/sci_pain3.wav");
