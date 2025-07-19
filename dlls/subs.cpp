4b3fd4058fd98df7da35790105495bddfccfa932
Xalalau <xalalau@outlook.com>
2025-07-19 15:03:52 -0300

    Adicionado point_counter (por @NickMBR)

--- dlls/subs.cpp
+++ dlls/subs.cpp
@@ -583,3 +583,121 @@ bool FEntIsVisible(
 
 	return false;
 }
+
+// ############ hu3lifezado ############ //
+/*
+	HU3-LIFE point_counter
+	Entidade que conta ativações para ativar o target.
+*/
+
+class CPointCounter : public CPointEntity
+{
+public:
+	using BaseClass = CPointEntity;
+
+#ifndef CLIENT_DLL
+	bool Save(CSave& save) override;
+	bool Restore(CRestore& restore) override;
+
+	static TYPEDESCRIPTION m_SaveData[];
+#endif
+
+void Spawn(void);
+	bool KeyValue(KeyValueData *pkvd) override;
+	virtual void Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value);
+
+	int m_iCount;
+	int m_iCountTotal;
+	string_t m_sMessageTitle;
+	string_t m_sMessageFinished;
+
+private:
+	hudtextparms_t m_sCountText;
+};
+
+#ifndef CLIENT_DLL
+TYPEDESCRIPTION CPointCounter::m_SaveData[] =
+{
+	DEFINE_FIELD(CPointCounter, m_iCount, FIELD_INTEGER),
+	DEFINE_FIELD(CPointCounter, m_sMessageTitle, FIELD_STRING),
+	DEFINE_FIELD(CPointCounter, m_sMessageFinished, FIELD_STRING),
+};
+
+IMPLEMENT_SAVERESTORE(CPointCounter, CPointCounter::BaseClass);
+#endif
+
+LINK_ENTITY_TO_CLASS(point_counter, CPointCounter);
+
+bool CPointCounter::KeyValue(KeyValueData *pkvd)
+{
+	// Pegamos a entrada flcount do hammer (fgd) e adicionamos a variavel.
+	if (FStrEq(pkvd->szKeyName, "picount"))
+	{
+		m_iCount = atof(pkvd->szValue);
+		return true;
+	}
+	else if (FStrEq(pkvd->szKeyName, "ptitle"))
+	{
+		m_sMessageTitle = ALLOC_STRING(pkvd->szValue);
+		return true;
+
+		// Configura os parametros do texto
+		// TODO: Deixar o jogador setar isso? (Copia do CGameText, nao reinventar a roda!..)
+		m_sCountText.x = -1;
+		m_sCountText.y = 0.8;
+		m_sCountText.effect = 2;
+		m_sCountText.channel = 4;
+
+		// Cores
+		m_sCountText.r1 = 210;
+		m_sCountText.g1 = 210;
+		m_sCountText.b1 = 210;
+		m_sCountText.a1 = 255;
+
+		m_sCountText.r2 = 240;
+		m_sCountText.g2 = 110;
+		m_sCountText.b2 = 0;
+		m_sCountText.a2 = 255;
+
+		// Tempo de Efeitos
+		m_sCountText.fadeinTime = 0.05;
+		m_sCountText.fadeoutTime = 0.5;
+		m_sCountText.holdTime = 15;
+		m_sCountText.fxTime = 0.10;
+	}
+	else if (FStrEq(pkvd->szKeyName, "pfinishedtitle"))
+	{
+		m_sMessageFinished = ALLOC_STRING(pkvd->szValue);
+		return true;
+	}
+	else
+		return CPointEntity::KeyValue(pkvd);
+}
+
+void CPointCounter::Spawn(void)
+{
+	// Se não tivermos uma contagem, resetamos para 1.
+	if (m_iCount <= 0) { m_iCount = 1; }
+	m_iCountTotal = m_iCount;
+}
+
+void CPointCounter::Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value)
+{
+	// Mostrar progresso
+	char buffer[100];
+	sprintf(buffer, "%s: %d de %d", STRING(m_sMessageTitle), (m_iCountTotal - m_iCount) + 1, m_iCountTotal);
+
+	UTIL_HudMessageAll(m_sCountText, buffer);
+
+	// Ao ativar, subtraimos 1 da contagem.
+	m_iCount--;
+
+	// Ao chegar em zero, ativamos o target final
+	if (m_iCount == 0)
+	{
+		UTIL_HudMessageAll(m_sCountText, STRING(m_sMessageFinished));
+		SUB_UseTargets(pActivator, USE_TOGGLE, 0);
+		m_iCount = 1;
+	}
+}
+// ############ //
