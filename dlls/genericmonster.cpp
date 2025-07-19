387befbe1fbcb78b8bd8922561f0015371dea4b0
Xalalau <xalalau@outlook.com>
2025-07-19 17:10:51 -0300

    Adicionado monster_generic_god

--- dlls/genericmonster.cpp
+++ dlls/genericmonster.cpp
@@ -139,3 +139,43 @@ void CGenericMonster::Precache()
 //=========================================================
 // AI Schedules Specific to this monster
 //=========================================================
+
+// ############ hu3lifezado ############ //
+// HU3-LIFE GENÉRICO INDISTRUTÍVEL
+
+class CGenericMonsterGod : public CGenericMonster
+{
+public:
+	void Spawn(void);
+};
+
+LINK_ENTITY_TO_CLASS(monster_generic_god, CGenericMonsterGod);
+
+void CGenericMonsterGod::Spawn()
+{
+	Precache();
+
+	SET_MODEL(ENT(pev), STRING(pev->model));
+
+	if (FStrEq(STRING(pev->model), "models/player.mdl") || FStrEq(STRING(pev->model), "models/holo.mdl"))
+		UTIL_SetSize(pev, VEC_HULL_MIN, VEC_HULL_MAX);
+	else
+		UTIL_SetSize(pev, VEC_HUMAN_HULL_MIN, VEC_HUMAN_HULL_MAX);
+
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_RED;
+	pev->health = 999999;
+	m_flFieldOfView = 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
+	m_MonsterState = MONSTERSTATE_NONE;
+
+	MonsterInit();
+
+	if (pev->spawnflags & SF_GENERICMONSTER_NOTSOLID)
+	{
+		pev->solid = SOLID_NOT;
+		pev->takedamage = DAMAGE_NO;
+	}
+}
+
+// ############ //
